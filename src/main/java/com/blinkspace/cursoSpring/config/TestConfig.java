f6f4a4f2c160a143553544635cc5b91a5891b6f6
MCFuhr82 <marcelinhofuhr@gmail.com>
2022-12-01 10:36:23 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/blinkspace/cursoSpring/config/TestConfig.java
+++ src/main/java/com/blinkspace/cursoSpring/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.blinkspace.cursoSpring.config;
 
 import com.blinkspace.cursoSpring.entities.Order;
 import com.blinkspace.cursoSpring.entities.User;
+import com.blinkspace.cursoSpring.entities.enums.OrderStatus;
 import com.blinkspace.cursoSpring.repositories.OrderRepository;
 import com.blinkspace.cursoSpring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Marcelo Fuhr", "marcelo@gmail.com", "9999999", "12345");
         User u2 = new User(null, "Rodrigo Fuhr", "rodrigo@gmail.com", "9999999", "12345");
 
-        Order o1 = new Order(null, Instant.parse("2022-11-15T15:23:25Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2022-11-20T15:23:25Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2022-11-25T15:23:25Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2022-11-15T15:23:25Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2022-11-20T15:23:25Z"), OrderStatus.DELIVERED, u2);
+        Order o3 = new Order(null, Instant.parse("2022-11-25T15:23:25Z"), OrderStatus.SHIPPED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
